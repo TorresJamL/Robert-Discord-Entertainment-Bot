b3d83743e857e6f7996443f4d9afcbd24c5698f1
Jamil Torres <torresjamil008@gmail.com>
2024-12-06 21:03:37 -0500

    Updated

--- RobertWorkspace/BotGame/BoardCog.py
+++ RobertWorkspace/BotGame/BoardCog.py
@@ -1,5 +1,23 @@
+import asyncio
+import discord
+import random
+import tracemalloc
+tracemalloc.start()
 
+from discord.ext import commands
 
 class Board:
-    def __init__(self, x, y, z):
-        self.board = []
+    def __init__(self, width, height):
+        self.board = [[ 0 for i in range(width)] for j in range(height)] # Creates a 3d array of dimensions x, y, z
+
+    def __str__(self):
+        display_board:str
+        for row in self.board:
+            for col in row:
+                display_board += str(col)
+            s += '\n'
+        return display_board
+    
+class Level:
+    def __init__(self, boards_per_lvl):
+        self.boards = []
